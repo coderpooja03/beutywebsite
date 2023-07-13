7059f4d2f8f32afce3f6dae31dee1d5e3a2641f2
coderpooja03 <108329488+coderpooja03@users.noreply.github.com>
2023-07-13 10:29:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// toggle use show and hide
+$(document).ready(function(){
+$('#menu').click(function(){
+     $(this).toggleClass('fa-times');
+     $('.navbar').toggleClass('nav-toggle');
+});
+$(window).on('scroll load',function(){
+     $('#menu').removeClass('fa-times');
+     $('.navbar').removeClass('nav-toggle');
+});
+});
+
